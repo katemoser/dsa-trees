867c3bc2a6eb83a2504de61c529fb5b4def52c96
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-04-13 12:31:43 -0700

    WIP minDepth method

--- binary-tree.js
+++ binary-tree.js
@@ -17,7 +17,41 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth(node = this.root) {
+    //the approach: breadth-first, recursively
+    //look at the tree, tier by tier until one 
+    //of the nodes doesn't have either a left or right attr (kidfree)
+    //that first node is the minimum depth
 
+    //create depthCount = 1
+    //create parent queue, root node goes in
+    //while loop (parentQueue.length)
+    //current = parentQueue.shift()
+    //create children queue
+    //check current.left and .right, put kids into childrenQueue
+    //increment depthCount
+
+    let depthCount = 1;
+
+    let parentQueue = [node];
+
+    while(parentQueue.length){
+      let current = queue.shift();
+
+      if(current.left === null && current.right === null){
+        //if the node is a leaf, yer a free elf!
+        depthCount++;
+        return depthCount;
+      }
+
+      while()
+      for(let child of [current.left, current.right]){
+        if(child !== null){
+          queue.push(child);
+        }
+      }
+      depthCount++;
+    }
+    
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
