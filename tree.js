2c2f1fcd37ffb990847e0469fd821d0787cf602f
Kate Moser <komoser@gmail.com>
2022-04-13 13:23:39 -0500

    wrote numGreater, countEvens, sumValues

--- tree.js
+++ tree.js
@@ -15,12 +15,40 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = 0;
+    let countStack = [this.root];
+
+    while( countStack.length){
+      //pop from the top of the stack
+      //add that value to the suum
+      //push current node's children to the stack.
+      //rinse and repeat
+      let current = countStack.pop();
+      sum += current.val;
+      for(let child of current.children){
+        countStack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let numEvens = 0;
+    let toVisitStack = [this.root];
+
+    while( toVisitStack.length){
+      let current = toVisitStack.pop();
+      if(current.val %2 === 0){
+        numEvens++;
+
+      }
+      for(let child of current.children){
+        toVisitStack.push(child);
+      }
+    }
+    return numEvens;
 
   }
 
@@ -28,7 +56,19 @@ class Tree {
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let numGreater = 0;
+    let toVisitStack = [this.root];
 
+    while(toVisitStack.length){
+      let current = toVisitStack.pop();
+      if(current.val > lowerBound){
+        numGreater++;
+      }
+      for(let child of current.children){
+        toVisitStack.push(child);
+      }
+    }
+    return numGreater;
   }
 }
 
