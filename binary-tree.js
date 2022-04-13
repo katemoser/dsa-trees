2fd7c849930a74c065994c3861c19dbbb9fc4524
Kate Moser <komoser@gmail.com>
2022-04-13 18:54:56 -0500

    working on binary tree methods

--- binary-tree.js
+++ binary-tree.js
@@ -16,7 +16,19 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth(node = this.root) {
+  minDepth(node = this.root, level = 0) {
+
+    // console.log("CURRENTLY LOOKING AT :", node ? node.val : "null");
+    
+    if(!node){
+      return 0
+    }
+
+    //add one for the level of the current node
+    return 1 + Math.min(
+      this.minDepth(node.left),
+      this.minDepth(node.right)
+    )
     //the approach: breadth-first, recursively
     //look at the tree, tier by tier until one 
     //of the nodes doesn't have either a left or right attr (kidfree)
@@ -30,27 +42,41 @@ class BinaryTree {
     //check current.left and .right, put kids into childrenQueue
     //increment depthCount
 
-    let depthCount = 1;
+    //base case: root is null
+    // if(node === null){
+    //   return level;
+    // }
 
-    let parentQueue = [node];
+    // if(node.left === null && node.right === null){
+    //   return level;
+    // }
+    // level++;
+    
+    // //recursion
+    // return Math.min(this.minDepth(node.left, level +1), this.minDepth(node.right, level +1))
+    
+    // let depthCount = 1;
 
-    while(parentQueue.length){
-      let current = queue.shift();
 
-      if(current.left === null && current.right === null){
-        //if the node is a leaf, yer a free elf!
-        depthCount++;
-        return depthCount;
-      }
+    // let parentQueue = [node];
 
-      while()
-      for(let child of [current.left, current.right]){
-        if(child !== null){
-          queue.push(child);
-        }
-      }
-      depthCount++;
-    }
+    // while(parentQueue.length){
+    //   let current = queue.shift();
+
+    //   if(current.left === null && current.right === null){
+    //     //if the node is a leaf, yer a free elf!
+    //     depthCount++;
+    //     return depthCount;
+    //   }
+
+    //   while()
+    //   for(let child of [current.left, current.right]){
+    //     if(child !== null){
+    //       queue.push(child);
+    //     }
+    //   }
+    //   depthCount++;
+    // }
     
   }
 
